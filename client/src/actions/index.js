9a3f7b3192c7a26b829fcedcb25efd5e1ec0d396
Alex Sherman <asherman.ca@gmail.com>
2017-12-05 22:03:13 -0800

    header connected to auth store

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+// redux thunk automatically passes(aka curries) dispatch to action creators that return functions instead of actions
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+// same as writing
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios.get('/api/current_user')
+//       .then(res => dispatch({type: FETCH_USER, payload: res}));
+//   }
+// }
