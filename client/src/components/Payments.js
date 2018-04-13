eb11ad0060558e495b3d79fe0e59ac33a9539e1d
Alex Sherman <asherman.ca@gmail.com>
2018-04-13 14:42:28 -0700

    stripe framework v1

---
+++ client/src/components/Payments.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+// token takes the callback function for receiving the confirmation
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
