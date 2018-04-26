7ecf2d812ad769445822b6ce809d8dddb91deba6
Alex Sherman <asherman.ca@gmail.com>
2018-04-26 15:30:04 -0700

    survey schema added

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,6 +4,7 @@ const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Emaily</h1>
+      <p>Collect feedback from your users</p>
     </div>
   );
 };
---
+++ routes/surveyRoutes.js
@@ -0,0 +1,7 @@
+//  get api/surveys
+//  post api/surveys/webhooks (record feedback from users)
+//  post api/surveys (create a new survey)
+// title
+// subject
+// body (text to show in the survey)
+// recipients
