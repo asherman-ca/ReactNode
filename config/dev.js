974ce3d89c00eabe3ddbc8d6c07681fc3a736d80
Alex Sherman <alex@Alexs-MacBook-Pro.local>
2018-08-29 15:26:17 -0700

    unignored dev keys

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: '418721495039-bdb6fe71a1bgdtiaqdk690i6bfe5ebla.apps.googleusercontent.com',
+    googleClientSecret: 'dblGepnRozi1NlsnFiMCx6il',
+    mongoURI: 'mongodb://reactnode:reactnode@ds127436.mlab.com:27436/reactnode',
+    cookieKey: 'asckm2312342lknmvfkdsn',
+    stripePublishableKey: 'pk_test_rJn5bx4oBBKtobVfLzHd8iJH',
+    stripeSecretKey: 'sk_test_wKIqBq315oL9HrG1sg3Ub5ML'
+  };
+  
